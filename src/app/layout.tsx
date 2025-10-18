dfea82cee947f9bda0e8b7b4c279e0d7273140e4
marcuscgc <marcuscgc@gmail.com>
2025-10-18 11:41:15 -0300

    second

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      suppressHydrationWarning//tire os erros e avisos de hidratação
       >
         {children}
       </body>
